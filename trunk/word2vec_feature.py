979e9a2c4d0dd96f790afe4e01781b11e84f2d9b
Mengdi Wang <wangmengdi@gmail.com>
2016-05-07 23:10:21 -0400

    refactor word2vec

--- trunk/word2vec_feature.py
+++ trunk/word2vec_feature.py
@@ -82,145 +82,57 @@ def replace_nan(s):
     return s
 
 
-#df_all = w2v_load_data()
-'''
-df_all = load_saved_pickles("final_model.p")
-df_all = df_all[["search_term","product_title","product_description","brand_parsed","material_parsed",
-                 "attribute_bullets_stemmed","attribute_stemmed","search_term_unstemmed","product_title",
-                 "product_description","brand","material","attribute_bullets","value"]]
-dump_df_all(df_all, "final_model.p")
-df_tmp = df_all.iloc[:2]
-df_tmp.to_csv("tmp_dump.csv")
-print(df_tmp)
-'''
-df_all = load_saved_pickles("final_model.p")
-df_all['search_term'] = df_all['search_term'].map(lambda x:replace_nan(x))
-df_all['product_title'] = df_all['product_title'].map(lambda x:replace_nan(x))
-df_all['product_description'] = df_all['product_description'].map(lambda x:replace_nan(x))
-df_all['brand_parsed'] = df_all['brand_parsed'].map(lambda x:replace_nan(x))
-df_all['material_parsed'] = df_all['material_parsed'].map(lambda x:replace_nan(x))
-df_all['attribute_bullets_stemmed'] = df_all['attribute_bullets_stemmed'].map(lambda x:replace_nan(x))
-df_all['attribute_stemmed'] = df_all['attribute_stemmed'].map(lambda x:replace_nan(x))
-
-df_all['search_term_unstemmed'] = df_all['search_term_unstemmed'].map(lambda x:replace_nan(x))
-df_all['product_title_unstemmed'] = df_all['product_title_unstemmed'].map(lambda x:replace_nan(x))
-df_all['product_description_unstemmed'] = df_all['product_description_unstemmed'].map(lambda x:replace_nan(x))
-df_all['brand'] = df_all['brand'].map(lambda x:replace_nan(x))
-df_all['material'] = df_all['material'].map(lambda x:replace_nan(x))
-df_all['attribute_bullets'] = df_all['attribute_bullets'].map(lambda x:replace_nan(x))
-df_all['value'] = df_all['value'].map(lambda x:replace_nan(x))
-
-
-#build a set of sentenxes in 4 way
-st = df_all["search_term"]
-pt = df_all["product_title"]
-pd = df_all["product_description"]
-br = df_all["brand_parsed"]
-mr = df_all["material_parsed"]
-ab = df_all["attribute_bullets_stemmed"]
-at = df_all["attribute_stemmed"]
-
-
-#st + pt +pd vocab
-t = list()
-for i in range(len(st)):
-    p = st[i].split()
-    t.append(p)
-
-for i in range(len(pt)):
-    p = pt[i].split()
-    t.append(p)
-
-for i in range(len(pd)):
-    p = pd[i].split()
-    t.append(p)
-
-
-for i in range(len(ab)):
-    p = ab[i].split()
-    t.append(p)
-
-for i in range(len(at)):
-    p = at[i].split()
-    t.append(p)
-
-gc.collect()
-print ("first vocab")
-#st conc pt conc pd vocab
-t1 = list()
-for i in range(len(st)):
-    p = st[i].split()+pt[i].split()+pd[i].split()+br[i].split()+mr[i].split()+ab[i].split()+at[i].split()
-    t1.append(p)
-
-gc.collect()
-print ("second vocab")
-
-#st + pt +pd +br + mr vocab w/o pars
-st1 = df_all["search_term_unstemmed"]
-pt1 = df_all["product_title"]
-pd1 = df_all["product_description"]
-br1 = df_all["brand"]
-mr1 = df_all["material"]
-ab1 = df_all["attribute_bullets"]
-at1 = df_all["value"]
-
-t2 = list()
-for i in range(len(st)):
-    p = st1[i].split()
-    t2.append(p)
-
-for i in range(len(pt)):
-    p = pt1[i].split()
-    t2.append(p)
-
-for i in range(len(pd)):
-    p = pd1[i].split()
-    t2.append(p)
-
-
-for i in range(len(ab1)):
-    p = ab1[i].split()
-    t2.append(p)
-
-for i in range(len(at1)):
-    p = at1[i].split()
-    t2.append(p)
-
-gc.collect()
-print ("third vocab")
-
-#st conc pt conc pd conc br conc mr vocab w/o pars
-t3 = list()
-for i in range(len(st)):
-    p = st1[i].split()+pt1[i].split()+pd1[i].split()+br1[i].split()+mr1[i].split()+ab1[i].split()+at1[i].split()
-    t3.append(p)
-
-print ("fourth vocab")
-
-#trin models
-model0 = gensim.models.Word2Vec(t, sg=1, window=10, sample=1e-5, negative=5, size=300)
-model1 = gensim.models.Word2Vec(t1, sg=1, window=10, sample=1e-5, negative=5, size=300)
-model2 = gensim.models.Word2Vec(t2, sg=1, window=10, sample=1e-5, negative=5, size=300)
-model3 = gensim.models.Word2Vec(t3, sg=1, window=10, sample=1e-5, negative=5, size=300)
-#model4 = gensim.models.Word2Vec(t, sg=0,  hs=1, window=10,   size=300)
-#model5 = gensim.models.Word2Vec(t1, sg=0, hs=1,window=10,   size=300)
-#model6 = gensim.models.Word2Vec(t2, sg=0, hs=1, window=10,   size=300)
-#model7 = gensim.models.Word2Vec(t3, sg=0, hs=1,window=10,   size=300)
-
-print ("model prepared")
-gc.collect()
-
-
-#for each model calculate features^ n_similarity between st and something else
-model_list=[model0,model1,model2,model3]   #,model4  ,model5,model6,model7]
-n_sim=list()
-
-for model in model_list:
-
-    n_sim_pt=list()
+def build_model_1(st, pt, pd0, ab, at):
+    #st + pt +pd vocab
+    t = list()
+    st.map(lambda x: t.append(x))
+    pt.map(lambda x: t.append(x))
+    pd0.map(lambda x: t.append(x))
+    ab.map(lambda x: t.append(x))
+    at.map(lambda x: t.append(x))
+    model0 = gensim.models.Word2Vec(t, sg=1, window=10, sample=1e-5, negative=5, size=300)
+    '''
     for i in range(len(st)):
-        w1=st[i].split()
-        w2=pt[i].split()
+         p = st[i].split()
+        t.append(p)
+
+    for i in range(len(pt)):
+        p = pt[i].split()
+        t.append(p)
+
+    for i in range(len(pd)):
+        p = pd[i].split()
+        t.append(p)
+
+
+    for i in range(len(ab)):
+        p = ab[i].split()
+        t.append(p)
+
+    for i in range(len(at)):
+        p = at[i].split()
+        t.append(p)
+    '''
+    print ("first vocab Done")
+    return model0
+
+
+def build_model_2(st, pt, tpd, br, mr, ab, at):
+    #st conc pt conc pd vocab
+    t1 = list()
+    for i in range(len(st)):
+        p = st[i]+pt[i]+tpd[i]+br[i]+mr[i]+ab[i]+at[i]
+        t1.append(p)
+
+    model1 = gensim.models.Word2Vec(t1, sg=1, window=10, sample=1e-5, negative=5, size=300)
+    print ("second vocab Done")
+    return model1
+
+def get_sim_between_models(model, search, given):
+    n_sim=list()
+    for i in range(len(search)):
+        w1=search[i]
+        w2=given[i]
         d1=[]
         d2=[]
         for j in range(len(w1)):
@@ -230,51 +142,17 @@ for model in model_list:
             if w2[j] in model.vocab:
                 d2.append(w2[j])
         if d1==[] or d2==[]:
-            n_sim_pt.append(0)
+            n_sim.append(0)
         else:
-            n_sim_pt.append(model.n_similarity(d1,d2))
-    n_sim.append(n_sim_pt)
+            n_sim.append(model.n_similarity(d1,d2))
+    return n_sim
 
-    n_sim_pd=list()
-    for i in range(len(st)):
-        w1=st[i].split()
-        w2=pd[i].split()
-        d1=[]
-        d2=[]
-        for j in range(len(w1)):
-            if w1[j] in model.vocab:
-                d1.append(w1[j])
-        for j in range(len(w2)):
-            if w2[j] in model.vocab:
-                d2.append(w2[j])
-        if d1==[] or d2==[]:
-            n_sim_pd.append(0)
-        else:
-            n_sim_pd.append(model.n_similarity(d1,d2))
-    n_sim.append(n_sim_pd)
 
-    n_sim_at=list()
-    for i in range(len(st)):
-        w1=st[i].split()
-        w2=at[i].split()
-        d1=[]
-        d2=[]
-        for j in range(len(w1)):
-            if w1[j] in model.vocab:
-                d1.append(w1[j])
-        for j in range(len(w2)):
-            if w2[j] in model.vocab:
-                d2.append(w2[j])
-        if d1==[] or d2==[]:
-            n_sim_at.append(0)
-        else:
-            n_sim_at.append(model.n_similarity(d1,d2))
-    n_sim.append(n_sim_at)
-
+def get_sim_all(model, pt, pd0, br, mr, ab, at):
     n_sim_all=list()
     for i in range(len(st)):
-        w1=st[i].split()
-        w2=pt[i].split()+pd[i].split()+br[i].split()+mr[i].split()+ab[i].split()+at[i].split()
+        w1=st[i]
+        w2=pt[i]+pd0[i]+br[i]+mr[i]+ab[i]+at[i]
         d1=[]
         d2=[]
         for j in range(len(w1)):
@@ -287,55 +165,96 @@ for model in model_list:
             n_sim_all.append(0)
         else:
             n_sim_all.append(model.n_similarity(d1,d2))
-    n_sim.append(n_sim_all)
-
-    n_sim_all1=list()
-    for i in range(len(st)):
-        w1=st1[i].split()
-        w2=pt1[i].split()+pd1[i].split()+br1[i].split()+mr1[i].split()+ab1[i].split()+at1[i].split()
-        d1=[]
-        d2=[]
-        for j in range(len(w1)):
-            if w1[j] in model.vocab:
-                d1.append(w1[j])
-        for j in range(len(w2)):
-            if w2[j] in model.vocab:
-                d2.append(w2[j])
-        if d1==[] or d2==[]:
-            n_sim_all1.append(0)
-        else:
-            n_sim_all1.append(model.n_similarity(d1,d2))
-    n_sim.append(n_sim_all1)
-
-    n_sim_ptpd=list()
-    for i in range(len(st)):
-        w1=pt[i].split()
-        w2=pd[i].split()
-        d1=[]
-        d2=[]
-        for j in range(len(w1)):
-            if w1[j] in model.vocab:
-                d1.append(w1[j])
-        for j in range(len(w2)):
-            if w2[j] in model.vocab:
-                d2.append(w2[j])
-        if d1==[] or d2==[]:
-            n_sim_ptpd.append(0)
-        else:
-            n_sim_ptpd.append(model.n_similarity(d1,d2))
-    n_sim.append(n_sim_ptpd)
-    print ("model features done")
-
-st_names=["id"]
-for j in range(len(n_sim)):
-    df_all["word2vec_"+str(j)]=n_sim[j]
-    st_names.append("word2vec_"+str(j))
-
-#save features
-
-b=df_all[st_names]
-b.to_csv("features/df_word2vec_new.csv", index=False)
-gc.collect()
+    return n_sim_all
 
 
+def run():
+    #df_all = w2v_load_data()
+    '''
+    df_all = load_saved_pickles("final_model.p")
+    df_all = df_all[["search_term","product_title","product_description","brand_parsed","material_parsed",
+                     "attribute_bullets_stemmed","attribute_stemmed","search_term_unstemmed","product_title",
+                     "product_description","brand","material","attribute_bullets","value"]]
+    dump_df_all(df_all, "final_model.p")
+    df_tmp = df_all.iloc[:2]
+    df_tmp.to_csv("tmp_dump.csv")
+    print(df_tmp)
+    '''
+    df_all = load_saved_pickles("final_model.p")
+    df_all['search_term'] = df_all['search_term'].map(lambda x:replace_nan(x))
+    df_all['product_title'] = df_all['product_title'].map(lambda x:replace_nan(x))
+    df_all['product_description'] = df_all['product_description'].map(lambda x:replace_nan(x))
+    df_all['brand_parsed'] = df_all['brand_parsed'].map(lambda x:replace_nan(x))
+    df_all['material_parsed'] = df_all['material_parsed'].map(lambda x:replace_nan(x))
+    df_all['attribute_bullets_stemmed'] = df_all['attribute_bullets_stemmed'].map(lambda x:replace_nan(x))
+    df_all['attribute_stemmed'] = df_all['attribute_stemmed'].map(lambda x:replace_nan(x))
 
+    df_all['search_term_unstemmed'] = df_all['search_term_unstemmed'].map(lambda x:replace_nan(x))
+    df_all['product_title_unstemmed'] = df_all['product_title_unstemmed'].map(lambda x:replace_nan(x))
+    df_all['product_description_unstemmed'] = df_all['product_description_unstemmed'].map(lambda x:replace_nan(x))
+    df_all['brand'] = df_all['brand'].map(lambda x:replace_nan(x))
+    df_all['material'] = df_all['material'].map(lambda x:replace_nan(x))
+    df_all['attribute_bullets'] = df_all['attribute_bullets'].map(lambda x:replace_nan(x))
+    df_all['value'] = df_all['value'].map(lambda x:replace_nan(x))
+
+    # build a set of sentenxes in 4 way
+    st = df_all["search_term"].map(lambda x:x.split())
+    pt = df_all["product_title"].map(lambda x:x.split())
+    pd0 = df_all["product_description"].map(lambda x:x.split())
+    br = df_all["brand_parsed"].map(lambda x:x.split())
+    mr = df_all["material_parsed"].map(lambda x:x.split())
+    ab = df_all["attribute_bullets_stemmed"].map(lambda x:x.split())
+    at = df_all["attribute_stemmed"].map(lambda x:x.split())
+    
+    # st + pt +pd +br + mr vocab w/o pars
+    st1 = df_all["search_term_unstemmed"].map(lambda x:x.split())
+    pt1 = df_all["product_title"].map(lambda x:x.split())
+    pd1 = df_all["product_description"].map(lambda x:x.split())
+    br1 = df_all["brand"].map(lambda x:x.split())
+    mr1 = df_all["material"].map(lambda x:x.split())
+    ab1 = df_all["attribute_bullets"].map(lambda x:x.split())
+    at1 = df_all["value"].map(lambda x:x.split())
+
+    model0 = build_model_1(st, pt, pd0, ab, at)
+    dump_df_all(model0, "model0.p")
+    model1 = build_model_2(st, pt, pd0, br, mr, ab, at)
+    dump_df_all(model1, "model1.p")
+    model2 = build_model_1(st1, pt1, pd1, ab1, at1)
+    dump_df_all(model2, "model2.p")
+    model3 = build_model_2(st1, pt1, pd1, br1, mr1, ab1, at1)
+    dump_df_all(model3, "model3.p")
+    print ("model prepared")
+
+    #for each model calculate features^ n_similarity between st and something else
+    model_list=[model0,model1,model2,model3]
+    n_sim=list()
+
+    for model in model_list:
+        n_sim_pt=list()
+        n_sim_pt = get_sim_between_models(model, st, pt)
+        n_sim.append(n_sim_pt)
+        n_sim_pd = get_sim_between_models(model, st, pd0)
+        n_sim.append(n_sim_pd)
+        n_sim_at = get_sim_between_models(model, st, at)
+        n_sim.append(n_sim_at)
+        n_sim_ptpd = get_sim_between_models(model, pt, pd0)
+        n_sim.append(n_sim_ptpd)
+        n_sim_all=get_sim_all(model, pt, pd0, br, mr, ab, at)
+        n_sim.append(n_sim_all)
+        n_sim_all1=get_sim_all(model, pt1, pd1, br1, mr1, ab1, at1)
+        n_sim.append(n_sim_all1)
+        
+        print ("model features done")
+
+    st_names = ["id"]
+    for j in range(len(n_sim)):
+        df_all["word2vec_"+str(j)]=n_sim[j]
+        st_names.append("word2vec_"+str(j))
+
+    #save features
+    b=df_all[st_names]
+    b.to_csv("features/df_word2vec_new.csv", index=False)
+
+
+if __name__ == "__main__":
+    run()
